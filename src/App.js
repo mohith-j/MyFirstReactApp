aaa67af1ed243b652c4bfdacffb436a516abe4c2
Mohith Jothi Kannan <jothiteenu@gmail.com>
2022-12-13 00:39:23 -0700

    added venue details

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import './resources/styles.css';
 import Header from './components/header_footer/Header';
 import Footer from "./components/header_footer/Footer";
 import Featured from "./components/featured";
+import VenueInfo from "./components/venueInfo";
 
 const App=()=> {
   return (
     <div className="App">
       <Header/>
 
-      <><Featured/></>
+      <Featured/>
 
+      <VenueInfo/>
       <div style={{backgroundColor:'violet', height: '800px'}}></div>
       <div style={{backgroundColor:'yellow', height: '800px'}}></div>
       <div style={{backgroundColor:'orange', height: '800px'}}></div>
